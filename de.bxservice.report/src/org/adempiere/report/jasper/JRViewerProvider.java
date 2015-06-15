2044122fd5e04ff14b2cc3323c556ed5c0e4dd6b
Diego Ruiz <12065321+d-ruiz@users.noreply.github.com>
2015-06-15 17:02:59 +0200

    Fix Provider dependencies

--- de.bxservice.report/src/org/adempiere/report/jasper/JRViewerProvider.java
+++
@@ -1,9 +0,0 @@
-package org.adempiere.report.jasper;
-
-import net.sf.jasperreports.engine.JRException;
-import net.sf.jasperreports.engine.JasperPrint;
-
-public interface JRViewerProvider {
-
-	public void openViewer(JasperPrint jasperPrint, String title) throws JRException;
-}
